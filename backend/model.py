20fea3e71f2602fc72974acc7a5dfcb2e1b70f0a
pikann <hvhai22@gmail.com>
2020-11-11 14:39:49 +0700

    update model.py

--- backend/model.py
+++ backend/model.py
@@ -1,54 +1,113 @@
 from pymongo import MongoClient
-from mongoengine import ObjectIdField, StringField, DateTimeField, ListField, IntField, Document, connect, ReferenceField
 from bson.objectid import ObjectId
 import datetime
 
 client=MongoClient('mongodb+srv://pikan:zpldDfKVSo1E6wDK@writedown.xtaf7.mongodb.net/')
 db=client.write_down
-connect(db='write_down', username='pikan', password='zpldDfKVSo1E6wDK', host='mongodb+srv://writedown.xtaf7.mongodb.net/')
 
-class Comment(Document):
-    _id=ObjectIdField(primary_key=True)
-    id_post=ObjectIdField()
-    id_parent_comment=ObjectIdField()
-    content=StringField(required=True)
-    created_date=DateTimeField(default=datetime.datetime.now)
-    created_by=ObjectIdField(required=True)
-    vote=IntField(default=0)
-    
-    def save(self):
-        super().save()
-        db['post'].update_one({'_id': self.id_post}, {'$push': {'list_comment':self._id}})
+class Comment():
+    def __init__(self, dict=None):
+        if (dict==None):
+            self._id=ObjectId()
+            self.content=""
+            self.created_date=datetime.datetime.now()
+            self.created_by=None
+            self.list_comment=[]
+            self.vote=0
+        else:
+            self._id=dict['_id']
+            self.content=dict['content']
+            self.created_date=dict['created_date']
+            self.created_by=dict['created_by']
+            self.list_comment=dict['list_comment']
+            self.vote=dict['vote']
 
-class Post(Document):
-    _id=ObjectIdField(default=ObjectId(), primary_key=True)
-    url_post=StringField()
-    title=StringField(required=True)
-    content=StringField(required=True)
-    created_date=DateTimeField(default=datetime.datetime.now)
-    list_hashtag=ListField(fields=StringField(), default=[])
-    category=ObjectIdField(required=True)
-    created_by=ObjectIdField(required=True)
-    time_to_read=IntField(default=5)
-    list_comment=ListField(fields=ObjectIdField, default=[])
-    vote=IntField(default=0)
+class Post():
+    def __init__(self, dict=None):
+        if (dict==None):
+            self._id=ObjectId()
+            self.url_post=""
+            self.title=""
+            self.content=""
+            self.created_date=datetime.datetime.now()
+            self.list_hashtag=[]
+            self.category=None
+            self.created_by=None
+            self.time_to_read=0
+            self.list_comment=[]
+            self.vote=0
+
+            self.db_saved=False
+
+        else:
+            self.db_saved=False
+            self._id=dict['_id']
+            self.url_post=dict['url_post']
+            self.title=dict['title']
+            self.content=dict['content']
+            self.created_date=dict['created_date']
+            self.list_hashtag=dict['list_hashtag']
+            self.category=dict['category']
+            self.created_by=dict['created_by']
+            self.time_to_read=dict['time_to_read']
+            self.list_comment=dict['list_comment']
+            self.vote=dict['vote']
+
+            self.db_saved=dict['db_saved']
+
+    def add_comment(self, comment, save_to_db=True):
+        self.list_comment.append(comment.__dict__)
+        if (self.db_saved and save_to_db):
+            db.post.update_one({'_id': self._id}, {'$push': {'list_comment': comment.__dict__}})
+
+    def insert(self):
+        self.db_saved=True
+        db.post.insert_one(self.__dict__)
+
+class User():
+    def __init__(self, dict=None):
+        if (dict==None):
+            self._id=ObjectId()
+            self.username=""
+            self.email=""
+            self.password=""
+            self.link_facebook=""
+            self.gender=""
+            self.avatar=""
+            self.birthday=datetime.date(1999, 1, 1)
+            self.bio=""
+            self.list_post=[]
+            self.list_draft=[]
+            self.list_category=[]
+
+            self.db_saved=False
+            
+        else:
+            self._id=dict['_id']
+            self.username=dict['username']
+            self.email=dict['email']
+            self.password=dict['password']
+            self.link_facebook=dict['link_facebook']
+            self.gender=dict['gender']
+            self.avatar=dict['avatar']
+            self.birthday=dict['birthday']
+            self.bio=dict['bio']
+            self.list_post=dict['list_post']
+            self.list_draft=dict['list_draft']
+            self.list_category=dict['list_category']
+
+            self.db_saved=dict['db_saved']
+
+    def add_post(self, post, save_to_db=True):
+        self.list_post.append(post.__dict__)
+        if (self.db_saved and save_to_db):
+            db.user.update_one({'_id': self._id}, {'$push': {'list_post': post.__dict__}})
+
+    def insert(self):
+        self.db_saved=True
+        db.user.insert_one(self.__dict__)
 
 if __name__=="__main__":
-    comment=Comment(_id=ObjectId(), 
-                id_post=ObjectId(db.post.find_one({})['_id']), 
-                content="asdfad",
-                created_by=ObjectId())
-    comment.save()
-
-    # post=Post(_id=ObjectId(), 
-    #             url_post="asdf", 
-    #             title="asdfasdg", 
-    #             content="asdfasd", 
-    #             created_date=datetime.datetime.now(), 
-    #             list_hashtag=[ObjectId()],
-    #             category=ObjectId(), 
-    #             created_by=ObjectId(), 
-    #             time_to_read=20, 
-    #             list_comment=[], 
-    #             vote=30)
-    # post.save()
+    post=Post(db.post.find_one({}))
+    comment=Comment()
+    post.add_comment(comment)
