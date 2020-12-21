504c52f26dcce3befde507648e2b32d941c62cca
nghialuffy <nghialuffydell@gmail.com>
2020-12-22 03:55:21 +0700

    chore: add socketio - add socketio into app.py - fix api get categories because Ms. Quyen so lam chuyen

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,19 @@
 import os
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-
 CONFIG_PATH = os.path.join(ROOT_DIR, 'configuration.conf')
 
+
+from flask import Blueprint, Flask
+from flask_socketio import SocketIO
+from flask_cors import CORS
+from pymongo import MongoClient
+import eventlet
+from eventlet import wsgi
 from controller import *
 
+app = Flask(__name__)
+cors = CORS(app)
+socketio = SocketIO(app, async_mode='eventlet')
 if __name__ == "__main__":
-    app = Flask(__name__)
     app.register_blueprint(bp)
-    app.run(host="0.0.0.0", debug=True)
+    socketio.run(app,host='0.0.0.0', debug=True)
