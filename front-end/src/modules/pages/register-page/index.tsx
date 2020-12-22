738e74be3c58800f96719b36c6ccba8f3c5c09bb
Lê V?n C?n <canleit5509@gmail.com>
2020-12-22 16:18:54 +0700

    add login page

--- front-end/src/modules/pages/register-page/index.tsx
+++ front-end/src/modules/pages/register-page/index.tsx
@@ -1,11 +1,16 @@
 import { Form, Input, Button, Checkbox } from 'antd';
-import React from 'react';
+import React, { useState } from 'react';
 import { AppWrap } from '../../../components';
 import './style.scss';
-
-
+import AuthService from "../../../service/user/auth";
+import { AxiosError, AxiosResponse } from 'axios';
+import { useHistory } from 'react-router-dom';
+import logo from './../../../assets/logo.png'
 export function RegisterPage() {
-    const [register] = Form.useForm();
+    const [register_form] = Form.useForm();
+    const [successful, setSuccessful] = useState(false);
+    const [message, setMessage] = useState("");
+    let history = useHistory();
     const layout = {
         labelCol: { span: 8 },
         wrapperCol: { span: 16 },
@@ -13,23 +18,38 @@ export function RegisterPage() {
     const tailLayout = {
         wrapperCol: { offset: 8, span: 16 },
     };
-    const onFinish = (values:any) => {
+    const onFinish = (values: any) => {
+        AuthService.register(values.username, values.password).then(
+            (response: AxiosResponse) => {
+                setMessage(response.data);
+                setSuccessful(true);
+            },
+            (error: AxiosError) => {
+                const resMessage =
+                    (error.response &&
+                        error.response.data &&
+                        error.response.data.message) ||
+                    error.message ||
+                    error.toString();
 
+                setMessage(resMessage);
+                setSuccessful(false);
+            }
+        );
+        console.log(message)
+        history.push("/");
     };
 
-    const onFinishFailed = (values:any) => {
-        console.log('Failed:', values);
-    };
 
     return (
         <AppWrap>
+            <img src={logo} alt='Write Down logo' className='logo' height='300' width = '300'/>
             <Form
-                form={register}
+                form={register_form}
                 {...layout}
-                name="register"
+                name="register_form"
                 initialValues={{ remember: true }}
                 onFinish={onFinish}
-                onFinishFailed={onFinishFailed}
             >
                 <Form.Item
                     label="Username"
@@ -42,7 +62,12 @@ export function RegisterPage() {
                 <Form.Item
                     label="Password"
                     name="password"
-                    rules={[{ required: true, message: 'Please input your password!' }]}
+                    dependencies={['password']}
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your password!'
+                        }]}
                 >
                     <Input.Password />
                 </Form.Item>
@@ -68,9 +93,7 @@ export function RegisterPage() {
                 >
                     <Input.Password />
                 </Form.Item>
-                <Form.Item {...tailLayout} name="remember" valuePropName="checked">
-                    <Checkbox>Remember me</Checkbox>
-                </Form.Item>
+
 
                 <Form.Item {...tailLayout}>
                     <Button type="primary" htmlType="submit">Submit</Button>
