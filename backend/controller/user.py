202219733dac089ba3836728f2a229bd5e94f9cd
pikann <hvhai22@gmail.com>
2020-12-22 17:10:33 +0700

    add feature follow

--- backend/controller/user.py
+++ backend/controller/user.py
@@ -34,5 +34,21 @@ def follow(username):
     else:
         abort(403)
 
+
+@bp.route('/user/<username>/unfollow', methods=['POST'])
+@token_auth.login_required
+def unfollow(username):
+    token = g.current_token.get_token()
+    user=db.user.find_one({"username": username}, {"_id": 1, "list_follow": 1})
+    if user["_id"]==token.id_user:
+        abort(403)
+    if str(token.id_user) not in user["list_follow"]:
+        abort(403)
+    e=db.user.update_one({"username": username}, {"$pull": {"list_follow": str(token.id_user)}})
+    if e.matched_count > 0:
+        return "ok"
+    else:
+        abort(403)
+
 if __name__ == "__main__":
     print(get_user("Araragikoyomioc"))
