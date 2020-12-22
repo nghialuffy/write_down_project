f5adb51b989f46b8df04e1b192c507ed0a938a2f
quyen.nguyen5 <quyen.nguyen@fiisoft.com>
2020-12-23 01:46:59 +0700

    update: top writer page

--- front-end/src/components/side-bar-right/top-month-posts/index.tsx
+++ front-end/src/components/side-bar-right/top-month-posts/index.tsx
@@ -3,14 +3,16 @@ import { useEntityDataList } from '../../../access';
 import { PostItemType } from '../../../model';
 import { BaseList } from '../../base';
 import { ComposeHeader, PostItem } from '../../index';
+import { LoadingFullView } from '../../loading';
 
 export function TopMonthPosts () {
-    const {loading, data} = useEntityDataList<PostItemType>('/postofmonth');
+    const {loading, data} = useEntityDataList<{data: PostItemType[]}>('postofmonth/');
     return (
         <div>
             <ComposeHeader>Bài viết của tháng</ComposeHeader>
             <div className='list-post-of-month'>
-                {data && <BaseList<PostItemType> data={data as any} Item={PostItem}/>}
+                {loading && <LoadingFullView className='tranparent'/>}
+                {data && <BaseList<PostItemType> data={data.data as any} Item={PostItem}/>}
             </div>
         </div>
     );
