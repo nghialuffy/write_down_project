492a46443545bb0abf93f3c54b5ef3960f064cb0
pikann <hvhai22@gmail.com>
2020-12-22 17:41:13 +0700

    update feature user

--- backend/controller/user.py
+++ backend/controller/user.py
@@ -4,28 +4,41 @@ from flask import g, abort
 from controller.auth import token_auth
 
 @bp.route('/user/<id>', methods=['GET'])
+@token_auth.login_required(optional=True)
 def get_user(id):
-    list_post = []
-    user = db.user.find_one({"_id": ObjectId(id)},
-                            {"password": 0, "link_facebook": 0, "email": 0, "list_draft": 0, "list_category": 0})
-    if(user != None):
-        if len(user["list_post"]) > 0:
-            for id_post in user["list_post"]:
-                post = Post(db.post.find_one({"_id": id_post}))
-                list_post.append(post.get_mini_post(get_username=False))
-        followings = len(list(db.user.find({"list_follow": user["_id"]})))
+    try:
+        list_post = []
+        user = db.user.find_one({"_id": ObjectId(id)},
+                                {"password": 0, "link_facebook": 0, "email": 0, "list_draft": 0, "list_category": 0})
+        if(user != None):
+            if len(user["list_post"]) > 0:
+                for id_post in user["list_post"]:
+                    post = Post(db.post.find_one({"_id": id_post}))
+                    list_post.append(post.get_mini_post(get_username=False))
+            followings = len(list(db.user.find({"list_follow": user["_id"]})))
 
-        return {
-                    "_id" : str(user["_id"]),
-                    "username": user["username"],
-                    "display_name": user["display_name"],
-                    "avatar": user["avatar"],
-                    "cover_img": user["cover_img"],
-                    "bio": user["bio"],
-                    "list_post": list_post,
-                    "followings": followings,
-                    "followers": len(user["list_follow"])
-                }
+            rs={
+                        "_id" : str(user["_id"]),
+                        "username": user["username"],
+                        "display_name": user["display_name"],
+                        "avatar": user["avatar"],
+                        "cover_img": user["cover_img"],
+                        "bio": user["bio"],
+                        "list_post": list_post,
+                        "followings": followings,
+                        "followers": len(user["list_follow"])
+                    }
+
+            if g.current_token is not None:
+                token = g.current_token.get_token()
+                if str(token.id_user) in user["list_follow"]:
+                    rs["followed"] = 1
+                else:
+                    rs["followed"] = 0
+
+            return rs
+    except:
+        abort(403)
 
 
 @bp.route('/user/<id>/follow', methods=['POST'])
