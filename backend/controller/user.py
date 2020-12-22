be9f26c061e3d500537322ef75dce7cc30d5ae28
pikann <hvhai22@gmail.com>
2020-12-22 16:47:17 +0700

    add feature auth

--- backend/controller/user.py
+++ backend/controller/user.py
@@ -1,6 +1,7 @@
 from controller import bp, db
 from controller.model import Post
-
+from controller.auth import token_auth
+from flask import g, abort
 
 @bp.route('/user/<username>', methods=['GET'])
 def get_user(username):
@@ -17,6 +18,21 @@ def get_user(username):
             "cover_img": user["cover_img"], "bio": user["bio"], "list_post": list_post,
             "followings": len(user["list_follow"]), "followers": followers}
 
+
+@bp.route('/user/<username>/follow', methods=['POST'])
+@token_auth.login_required
+def follow(username):
+    token = g.current_token.get_token()
+    user=db.user.find_one({"username": username}, {"_id": 1, "list_follow": 1})
+    if user["_id"]==token.id_user:
+        abort(403)
+    if str(token.id_user) in user["list_follow"]:
+        abort(403)
+    e=db.user.update_one({"username": username}, {"$push": {"list_follow": str(token.id_user)}})
+    if e.matched_count > 0:
+        return "ok"
+    else:
+        abort(403)
 
 if __name__ == "__main__":
     print(get_user("Araragikoyomioc"))
