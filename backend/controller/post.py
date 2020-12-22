1b8eda33ae28cb9c7eb8a62f1b1eb550fdb51fac
nghialuffy <nghialuffydell@gmail.com>
2020-12-22 23:10:52 +0700

    chore: fix post/<id> - fix category, created_by in post/<id> - modified top_user

--- backend/controller/post.py
+++ backend/controller/post.py
@@ -28,29 +28,45 @@ def get_comment(data_cmt, is_reply=False):
 def get_post(id):
     try:
         post = {}
-        data = db.post.find_one({"_id": ObjectId(id)}, {"_id": 0, "url_post": 0, "time_to_read": 0})
-        post["title"] = data["title"]
-        post["content"] = data["content"]
-        post["created_date"] = data["created_date"]
-        post["list_hashtag"] = data["list_hashtag"]
-        post["vote"] = data["vote"]
-        post["views"] = data["views"] + 1
-        post["edit_history"] = len(data["edit_history"]) > 0
-        post["category"] = db.category.find_one({"_id": data["category"]}, {"_id": 0, "name_category": 1, "url": 1})
-        post["created_by"] = db.user.find_one({"_id": data["created_by"]}, {"_id": 0, "username": 1, "display_name": 1,
-                                                                            "avatar": 1})
-        post["list_comment"] = []
-        for data_cmt in data["list_comment"]:
-            post["list_comment"].append(get_comment(data_cmt))
+        data = db.post.find_one({"_id": ObjectId(id)}, {"url_post": 0, "time_to_read": 0})
+        if data != None:
+            post["_id"] = str(data["_id"])
+            post["title"] = data["title"]
+            post["content"] = data["content"]
+            post["created_date"] = data["created_date"]
+            post["list_hashtag"] = data["list_hashtag"]
+            post["vote"] = data["vote"]
+            post["views"] = data["views"] + 1
+            post["edit_history"] = len(data["edit_history"]) > 0
 
-        if g.current_token is not None:
-            token = g.current_token.get_token()
-            if str(token.id_user) in data["voted_user"]:
-                post["user_voted"] = data["voted_user"][str(token.id_user)]
+            post["category"] = {}
+            query1 = db.category.find_one({"_id": ObjectId(data["category"])})
+            if(query1 != None):
+                post["category"]["_id"] = str(query1["_id"])
+                post["category"]["name_category"] = query1["name_category"]
+                post["category"]["url"] = query1["url"]
 
-        db.post.update_one({"_id": ObjectId(id)}, {"$set": {"views": data["views"] + 1}})
-        return post
-    except:
+            post["created_by"] = {}
+            query2 = db.user.find_one({"_id": ObjectId(data["created_by"])})
+            if(query2 != None):
+                post["created_by"]["_id"] = str(query2["_id"])
+                post["created_by"]["username"] = query2["username"]
+                post["created_by"]["display_name"] = query2["display_name"]
+                post["created_by"]["avatar"] = query2["avatar"]
+            
+            post["list_comment"] = []
+            for data_cmt in data["list_comment"]:
+                post["list_comment"].append(get_comment(data_cmt))
+
+            if g.current_token is not None:
+                token = g.current_token.get_token()
+                if str(token.id_user) in data["voted_user"]:
+                    post["user_voted"] = data["voted_user"][str(token.id_user)]
+
+            db.post.update_one({"_id": ObjectId(id)}, {"$set": {"views": data["views"] + 1}})
+            return post
+    except Exception as exc:
+        print(f"Error: {exc}")
         abort(403)
 
 
