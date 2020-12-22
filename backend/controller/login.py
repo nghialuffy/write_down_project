127fdc4535c06f98de30cb50db8d4e43797c0203
pikann <hvhai22@gmail.com>
2020-12-22 15:47:18 +0700

    add feature logout

--- backend/controller/login.py
+++ backend/controller/login.py
@@ -1,9 +1,18 @@
-from flask import jsonify, g
+from flask import jsonify, g, abort
 from controller import bp
-from controller.auth import basic_auth
+from controller.auth import basic_auth, token_auth
 
 @bp.route('/login', methods=['POST'])
 @basic_auth.login_required
 def login():
     token = g.current_token.get_token()
     return jsonify({'token': token._id})
+
+@bp.route('/logout', methods=['DELETE'])
+@token_auth.login_required
+def logout():
+    token = g.current_token.get_token()
+    if token.revoke_token()=="ok":
+        return "ok"
+    else:
+        abort(403)
